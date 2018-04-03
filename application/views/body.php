33709d062a4ed437d5f240909e5b20de93a09091
unknown <@.(none)>
2018-04-04 02:15:23 +0900

    Ŀ

---
+++ application/views/body.php
@@ -0,0 +1,12 @@
+<div id="wrapper_main">
+<section class="main_banner">
+<div class="main_banner_rolling">
+ <ul>
+  <li><img src="#" alt="메인이미지1"></li>
+
+ </ul>
+</div>
+echo dd;
+
+</section>
+</div>
